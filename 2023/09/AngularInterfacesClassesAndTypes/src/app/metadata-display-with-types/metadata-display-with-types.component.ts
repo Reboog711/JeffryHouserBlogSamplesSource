019d0d63039e8f987146b9ea9b3e7252ac1d31dd
Jeffry Houser <jeffry@dot-com-it.com>
2023-09-30 20:50:53 -0400

    Fleshing out the Interface with Types

--- 2023/09/AngularInterfacesClassesAndTypes/src/app/metadata-display-with-types/metadata-display-with-types.component.ts
+++ 2023/09/AngularInterfacesClassesAndTypes/src/app/metadata-display-with-types/metadata-display-with-types.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {MetadataType} from '../types/metadata-type';
 
 @Component({
   selector: 'app-metadata-display-with-types',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./metadata-display-with-types.component.css']
 })
 export class MetadataDisplayWithTypesComponent {
+  @Input()
+  data?: MetadataType;
 
 }
